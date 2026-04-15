de833cf03aece6a5aa5fe1bbc556817237dee49a
adamix <adamburdik9@gmail.com>
2026-04-15 16:58:28 +0200

    Update: twelve-hour-converter

--- twelve-hour-converter/main.c
+++ twelve-hour-converter/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-#define BUFFER_SIZE 1024
-#define FILEINPUT "time.txt"
+#define INPUT_FILENAME "time.txt"
+#define OUTPUT_FILENAME "odpoledne.txt"
+
+#define SEPERATOR_LENGTH 61
 
 typedef struct {
 	int hours;
@@ -14,9 +16,9 @@ typedef struct {
 	int minutes;
 	int seconds;
 	int half; // 0 == AM, 1 == PM
-} STUPID_TIME;
+} TwelveHourTime;
 
-int convert_for_stupid(TIME *input, STUPID_TIME *output) {
+int time_to_12hour(TIME *input, TwelveHourTime *output) {
     if (input == NULL || output == NULL) {
         return 1;
     }
@@ -46,67 +48,88 @@ int get_seconds_from_midnight(TIME *time) {
 	return time -> hours * 3600 + time -> minutes * 60 + time -> seconds;
 }
 
-int read_input_file(
-	char *filename,
-	TIME* array,
-	int array_size
-) {
-	FILE *input = fopen(filename, "r");
-	if (!input) {
-		printf("%s count be read!\n", filename);
-		return -1;
+FILE* try_fopen(char *filename, char* mode) {
+	FILE *file = fopen(filename, mode);
+	if (file == NULL) {
+		printf("Unable to read %s\n", filename);
+	}
+	return file;
+}
+
+void print_seperator(FILE *output, int n) {
+	for (int i = 0; i < n; i++) {
+		fprintf(output, "-");
+	}
+	fprintf(output, "\n");
+}
+
+void print_header(FILE *output) {
+	print_seperator(output, SEPERATOR_LENGTH);
+	fprintf(output, "| Index | Europe Time | Stupid Time | Seconds from midnight |\n");
+	print_seperator(output, SEPERATOR_LENGTH);
+}
+
+bool read_time(FILE *input, TIME *time) {
+	return fscanf(input, "%d %d %d", &time -> hours, &time -> minutes, &time -> seconds) == 3;
+}
+
+void write_entry(int i, FILE *output, TIME *time) {
+	TwelveHourTime twelve_hour_time = {};
+	time_to_12hour(time, &twelve_hour_time);
+
+	int seconds = get_seconds_from_midnight(time);
+
+	fprintf(
+		output,
+		"| %4d. |   %02d:%02d:%02d  | %02d:%02d:%02d %s | %21d |\n",
+		i + 1,
+		time -> hours,
+		time -> minutes,
+		time -> seconds,
+		twelve_hour_time.hours,
+		twelve_hour_time.minutes,
+		twelve_hour_time.seconds,
+		twelve_hour_time.half == 0 ? "AM" : "PM",
+		seconds
+	);
+}
+
+int main(void) {
+
+	FILE *input = try_fopen(INPUT_FILENAME, "r");
+	FILE *output = try_fopen(OUTPUT_FILENAME, "w");
+	if (!input || !output) {
+	 	return 1;
 	}
 
 	TIME time = {};
 
+	print_header(stdout);
+	print_header(output);
+
 	int i = 0;
-	while (fscanf(input, "%d %d %d", &time.hours, &time.minutes, &time.seconds) == 3) {
-		if (i >= array_size) {
-			printf("Input file contains more elements that provider buffer of size %d can hold\n", array_size);
-			fclose(input);
-			return -1;
+	int written_to_file = 0;
+	while (read_time(input, &time)) {
+		write_entry(i, stdout, &time);
+
+		if (time.hours >= 12) {
+			write_entry(written_to_file, output, &time);
+			written_to_file++;
 		}
-		array[i] = time;
+		
 		i++;
 	}
+
+	print_seperator(stdout, SEPERATOR_LENGTH);
+	print_seperator(output, SEPERATOR_LENGTH);
+
+	printf("%d time entries has been read from %s\n", i, INPUT_FILENAME);
+	printf("%d time entries were written to %s\n", written_to_file, OUTPUT_FILENAME);
+	fprintf(output, "%d time entries were written to %s\n", written_to_file, OUTPUT_FILENAME);
+
 	fclose(input);
-	return i;
-}
-
-void print_entries(TIME *array, int array_size) {
-	printf("----------------------------------------------------------------\n");
-	printf("Time Index | Europe Time | Stupid Time | Seconds from midnight |\n");
-	printf("----------------------------------------------------------------\n");
-	STUPID_TIME stupid = {};
-	for (int i = 0; i < array_size; i++) {
-		TIME *time = &array[i];
-		convert_for_stupid(time, &stupid);
-		int seconds = get_seconds_from_midnight(time);
-		printf(
-			"%9d. |   %02d:%02d:%02d  | %02d:%02d:%02d %s | %21d |\n",
-			i + 1,
-			time -> hours,
-			time -> minutes,
-			time -> seconds,
-			stupid.hours,
-			stupid.minutes,
-			stupid.seconds,
-			stupid.half == 0 ? "AM" : "PM",
-			seconds
-		);
-	}
-	printf("----------------------------------------------------------------\n");
-}
-
-int main(void) {
+	fclose(output);
 	
-	TIME array[BUFFER_SIZE];
-
-	int entries_read = read_input_file(FILEINPUT, array, BUFFER_SIZE);
-	if (entries_read <= 1) return 1;
-	printf("Read %d time entries from %s\n", entries_read, FILEINPUT);
-
-	print_entries(array, entries_read);
 	
 	return 0;
 }
